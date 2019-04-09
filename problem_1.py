11008c55377a3c60db3b4a992381569e327794a1
Lise Stork <lise.stork@gmail.com>
2019-04-09 15:45:08 +0200

    problem 1

--- problem_1.py
+++ problem_1.py
@@ -1,7 +1,15 @@
-#Project Euler, problem 1. Lise Stork
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr  9 15:43:43 2019
 
+@author: lisestork
+"""
+
+#Problem 1: 
 #If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
 #Find the sum of all the multiples of 3 or 5 below 1000
+
 import time
 import numpy as np
 
